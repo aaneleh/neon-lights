ec1480bce9d5f74af968a4062d65992112e2d870
aaneleh <helenakurzgon@gmail.com>
2022-01-06 23:32:57 -0300

    light and dark mode

--- index.js
+++ index.js
@@ -1,3 +1,43 @@
+
+/* LIGHT / DARK MODE */
+const root = document.querySelector(':root');
+function darkMode(){
+    root.style.setProperty('--background-color', 'hsl(var(--hue), 16%, 8%)');
+    root.style.setProperty('--font-color', 'hsl(var(--hue), 0%, 100%)');
+    root.style.setProperty('--icons-color', 'hsl(var(--hue), 0%, 100%)');
+}
+function lightMode(){
+    root.style.setProperty('--background-color', 'hsl(var(--hue), 0%, 100%)');
+    root.style.setProperty('--font-color', 'hsl(var(--hue), 16%, 8%)');
+    root.style.setProperty('--icons-color', 'hsl(var(--hue), 16%, 8%)');
+}
+
+var lastMode;
+window.onload = function(){
+    lastMode = localStorage.getItem('mode');
+    
+    if(JSON.parse(lastMode)=="light"){
+        lightMode();
+    } else {
+        darkMode();
+    }
+}
+
+const cssEstilos = getComputedStyle(root);
+var BgColor;
+function changeMode(){
+    BgColor = String(cssEstilos.getPropertyValue('--background-color')).trim();
+    hue = String(cssEstilos.getPropertyValue('--hue')).trim();
+
+    if(BgColor == "hsl( "+hue+", 16%, 8%)") {
+        lightMode();
+        localStorage.setItem('mode', JSON.stringify("light"));
+    } else if (BgColor == "hsl( "+hue+", 0%, 100%)"){  
+        darkMode();
+        localStorage.setItem('mode', JSON.stringify("dark"));
+    }
+}
+
 /* SIDE BAR */
 const sidebar = document.getElementById('side-bar');
 
@@ -5,6 +45,8 @@ function toggleSidebar(){
     sidebar.classList.toggle('active');
 }
 
+
+/* HOME */
 const check_1 = document.getElementById('check-2'); /* i really dont know why they are backwards */
 const check_2 = document.getElementById('check-1');
 check_1.classList.add('checked');
@@ -22,15 +64,4 @@ function ImageTwo(){
     check_1.classList.remove('checked');
     image_1.style.right = "100%";
     image_2.style.left = "0%";
-}
-
-
-
-
-
-
-
-/* LIGHT / DARK MODE */
-function changeMode(){
-    alert('change mode');
 }
